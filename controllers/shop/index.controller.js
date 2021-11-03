2ccb3d687998c8f7d9cbc61d79073e2d3aaaeeb1
tuanstvn1215 <tuanstvn1215@gmail.com>
2021-11-03 12:30:05 +0700

    c

--- controllers/shop/index.controller.js
+++ controllers/shop/index.controller.js
@@ -7,17 +7,28 @@ class IndexController extends Controller {
     }
 
     getIndex = async (req, res) => {
-        const product = ProductModel.find()
+        const starproductsheader = await ProductModel.find({
+            category: req.query.category,
+            status: 1,
+        })
+            .sort({ price: -1 })
+            .limit(5)
+
         const starproducts = []
-        const productCategory = ProductCategoryModel.find({ status: 1 })
+        const productCategory = await ProductCategoryModel.find({ status: 1 })
         for (let index = 0; index < productCategory.length; index++) {
-            const element = productCategory[index]
-            let tempproducts = await ProductModel.find({ category: element })
-            starproducts.push(tempproducts)
+            const category = productCategory[index]
+            let tempproducts = await ProductModel.find({
+                category: category,
+                status: 1,
+            }).limit(4)
+
+            starproducts.push({ category, tempproducts })
         }
         console.log(starproducts)
-
         res.render('shop/index', {
+            productCategory: productCategory,
+            starproductsheader: starproductsheader,
             user: res.locals.user,
             starproducts: starproducts,
         })
